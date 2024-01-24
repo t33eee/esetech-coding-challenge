30994b2e57872324728fe83c9dc61775649f5bf9
t33eee <nyenkeroytalent@gmail.com>
2024-01-25 00:32:38 +0100

    first commit

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,32 +1,9 @@
 var express = require("express");
-const { db } = require("../firebase");
 var router = express.Router();
 
 /* GET users listing. */
-router.post("/", async function (req, res, next) {
-  try {
-    const data = {
-      name: "john doe",
-      state: "kano",
-      country: "Nigeria",
-    };
-
-    const result = await db.collection("users").doc().set(data);
-    console.log(result, "result");
-
-    res.status(200).json({
-      status: "success",
-      message: "succesfully created an new user",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      status: "error",
-      message: "error creating user",
-    });
-    next(error);
-  }
+router.get("/", function (req, res, next) {
+  res.send("user route");
 });
 
 module.exports = router;
